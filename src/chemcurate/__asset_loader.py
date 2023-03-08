04ae2c00441216e33d791076a6c60d4ab8e1b93f
Jacob Gerlach <jwgerlach00@gmail.com>
2023-03-07 21:47:53 -0500

    Made uniport_mapping_filtered accessible through the package (__init__)

--- src/chemcurate/__asset_loader.py
+++ src/chemcurate/__asset_loader.py
@@ -5,3 +5,6 @@ from yaml.loader import SafeLoader
 
 with resources.open_text('assets', 'uniprot_mapping.yaml') as f:
     uniprot_mapping = yaml.load(f, Loader=SafeLoader)
+    
+with resources.open_text('assets', 'uniprot_mapping_filtered.yaml') as f:
+    uniprot_mapping_filtered = yaml.load(f, Loader=SafeLoader)
--- src/chemcurate/__init__.py
+++ src/chemcurate/__init__.py
@@ -1,3 +1,3 @@
 from chemcurate.Chembl import Chembl
 from chemcurate.PubChem import PubChem
-from chemcurate.__asset_loader import uniprot_mapping
+from chemcurate.__asset_loader import uniprot_mapping, uniprot_mapping_filtered
