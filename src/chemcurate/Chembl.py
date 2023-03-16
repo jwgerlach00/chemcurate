74c6e3f4914307b83ff28f7396ff803a5684ce65
jwgerlach00 <jwgerlach00@gmail.com>
2023-03-16 14:18:15 -0400

    Fixed requests.get issues with Chembl due to url being too long by batching. Only have next page in GET for assays from targets and NOT in GET structures from mol_cheml_ids because this is ONE-TO-ONE

--- src/chemcurate/Chembl.py
+++ src/chemcurate/Chembl.py
@@ -1,12 +1,17 @@
 import requests
 import pandas as pd
+from typing import Union, List, Generator, Iterable
 
 
 class Chembl:
-    url_stem = 'https://www.ebi.ac.uk'
+    _url_stem = 'https://www.ebi.ac.uk'
+    _batch_size = 100 # NOTE: DONT CHANGE (Chembl API limit)
     
-    def __init__(self, uniprot_ids:list):
-        target_chembl_ids = [Chembl.uniprot_2_chembl_target_id(i) for i in uniprot_ids]
+    def __init__(self, uniprot_ids:List[str]) -> None:
+        target_chembl_ids = []
+        for i in uniprot_ids:
+            target_chembl_ids += Chembl.uniprot_2_chembl_target_id(i)
+
         target_chembl_ids = [i for i in target_chembl_ids if i] # Remove None values
         if target_chembl_ids:
             self.df = Chembl.get_assays_from_target_chembl_ids(target_chembl_ids)
@@ -15,65 +20,77 @@ class Chembl:
         else:
             self.df = pd.DataFrame() # Empty dataframe if there is no data
         
-    def __str__(self):
+    def __str__(self) -> str:
         return str(self.df)
     
     def __len__(self) -> int:
         return len(self.df)
     
     @staticmethod
-    def uniprot_2_chembl_target_id(uniprot_id:str):
-        url = f'{Chembl.url_stem}/chembl/api/data/chembl_id_lookup/search.json?q={uniprot_id}'
-        response_json = requests.get(url).json()
-        if response_json['chembl_id_lookups']:
-            return response_json['chembl_id_lookups'][0]['chembl_id']
-        else:
-            return None
+    def batch(iterable:Iterable, n:int=1) -> Generator:
+        l = len(iterable)
+        for ndx in range(0, l, n):
+            yield iterable[ndx:min(ndx + n, l)]
     
     @staticmethod
-    def get_assays_from_target_chembl_ids(target_chembl_ids:list, limit:int=100) -> pd.DataFrame:
-        url = '{0}/chembl/api/data/activity.json?target_chembl_id__in={1}&assay_type=B&limit={2}'\
-            .format(Chembl.url_stem, ','.join(target_chembl_ids), limit)
-        response = requests.get(url)
+    def uniprot_2_chembl_target_id(uniprot_id:str) -> Union[List[str], None]:
+        url = f'{Chembl._url_stem}/chembl/api/data/chembl_id_lookup/search.json?q={uniprot_id}'
+        response_json = requests.get(url).json()
+        if response_json['chembl_id_lookups']:
+            return [target['chembl_id'] for target in response_json['chembl_id_lookups']]
+        else:
+            return None
         
+    @staticmethod
+    def get_assays_from_target_chembl_ids(target_chembl_ids:List[str]) -> pd.DataFrame:
         activities = []
-        activities += response.json()['activities']
-        
-        # Loop through remaining entries
-        while response.json()['page_meta']['next']:
-            response = requests.get('{0}{1}'.format(Chembl.url_stem, response.json()['page_meta']['next']))
-            activities += response.json()['activities']
+        # Batch in case targets are greater than Chembl._batch_size
+        for x in Chembl.batch(target_chembl_ids, Chembl._batch_size):
+            url = '{0}/chembl/api/data/activity.json?target_chembl_id__in={1}&assay_type=B&limit={2}'\
+                .format(Chembl._url_stem, ','.join(x), Chembl._batch_size)
+            
+            response_json = requests.get(url).json()
+            activities += response_json['activities']
+            
+            # Loop through remaining entries
+            while response_json['page_meta']['next']:
+                response_json = requests.get('{0}{1}'.format(Chembl._url_stem,
+                                                             response_json['page_meta']['next'])).json()
+                activities += response_json['activities']
         
         columns_to_include = ['target_chembl_id', 'target_organism', 'target_pref_name', 'molecule_chembl_id',
                             'molecule_pref_name', 'pchembl_value', 'standard_type', 'standard_relation',
                             'standard_value', 'standard_units', 'assay_chembl_id','document_chembl_id', 'src_id']
         
         return pd.DataFrame(activities, columns=columns_to_include)
-
+    
     @staticmethod
-    def get_structures_from_mol_chembl_ids(mol_chembl_ids:list, limit:int=100) -> pd.DataFrame:
-        url = '{0}/chembl/api/data/molecule.json?molecule_chembl_id__in={1}&limit={2}'\
-            .format(Chembl.url_stem, ','.join(mol_chembl_ids), limit)
-            
-        response = requests.get(url)
+    def get_structures_from_mol_chembl_ids(mol_chembl_ids:List[str]) -> pd.DataFrame:
+        unique_mol_chembl_ids = list(set(mol_chembl_ids))
         
         molecules = []
-        molecules += response.json()['molecules']
+        for x in Chembl.batch(unique_mol_chembl_ids, Chembl._batch_size):
+            url = '{0}/chembl/api/data/molecule.json?molecule_chembl_id__in={1}&limit={2}'\
+                .format(Chembl._url_stem, ','.join(x), Chembl._batch_size)
+            response_json = requests.get(url).json()
+            molecules += response_json['molecules']
+            # NOTE: No next page because retults = limit (one-to-one)
         
-        # Loop through remaining entries
-        while response.json()['page_meta']['next']:
-            response = requests.get('{0}{1}'.format(Chembl.url_stem, response.json()['page_meta']['next']))
-            molecules += response.json()['molecules']
-        
-        canonical_smiles = [m['molecule_structures']['canonical_smiles'] for m in molecules]
-        mol_chembl_ids = [m['molecule_chembl_id'] for m in molecules]
-        
-        return pd.DataFrame({'molecule_chembl_id': mol_chembl_ids, 'canonical_smiles': canonical_smiles})
+        mcid = []
+        cs = []
+        [(mcid.append(m['molecule_chembl_id']),
+          cs.append(m['molecule_structures']['canonical_smiles']))
+         for m in molecules]
+            
+        return pd.DataFrame({
+            'molecule_chembl_id': mcid,
+            'canonical_smiles': cs
+        })
 
 
 if __name__ == '__main__':
-    # target_id = 'CHEMBL2490'
-    target_id = 'P50129'
-    chembl = Chembl([target_id])
+    target_id_1 = 'P50129' # chembl_id = CHEMBL2490
+    target_id_2 = 'Q13936'
+    chembl = Chembl([target_id_1, target_id_2])
     print(chembl.df)
     print(len(chembl))
