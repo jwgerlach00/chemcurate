172c4eb151d239047b919727720eb3799b2f2b2e
Jacob Gerlach <jwgerlach00@gmail.com>
2023-03-30 16:21:16 -0400

    Fixed issues w/ making the dict in the cross_ref_mapper

--- src/assets/cross_reference_uniprot_mapper.py
+++ src/assets/cross_reference_uniprot_mapper.py
@@ -3,25 +3,26 @@ import joblib
 # from multiprocessing import Pool, cpu_count
 import os
 
-def process_organism(self, organism:str):
+def process_organism(organism:str):
     print(organism)
-    common_name = self.full_uniprot_mapping[organism]['common_name']
-    protein_names = [id for li in self.full_uniprot_mapping[organism]['protein'].keys() for id in li]
-    uniprot_ids = [id for li in self.full_uniprot_mapping[organism]['protein'].values() for id in li]
+    common_name = full_uniprot_mapping[organism]['common_name']
+    protein_names = full_uniprot_mapping[organism]['protein'].keys()
+    uniprot_ids = [id for li in full_uniprot_mapping[organism]['protein'].values() for id in li]
     
     protein_dict = {}
+    protein_dict['common_name'] = common_name
+    protein_dict['protein'] = {}
     for protein_name in protein_names:
         uniprot_list = []
         for uniprot_id in uniprot_ids:
-            booly = (uniprot_id in self.chembl_uniprot_ids, uniprot_id in self.pubchem_uniprot_ids)
+            booly = (uniprot_id in chembl_uniprot_ids, uniprot_id in pubchem_uniprot_ids)
             if sum(booly):
                 uniprot_list.append({uniprot_id: {
                     'in_chembl': booly[0],
                     'in_pubchem': booly[1]
                 }})
         if uniprot_list:
-            protein_dict[protein_name]['common_name'] = common_name
-            protein_dict[protein_name]['protein'] = uniprot_list
+            protein_dict['protein'][protein_name] = uniprot_list
 
     if protein_dict:
         with open('cross_referenced_uniprot_mapping.yaml', 'a') as f:
